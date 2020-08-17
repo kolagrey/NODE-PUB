1e965097c1399f07f22fa9abb1e9bd7f37c4e949
Kola Grey <kolagrey@gmail.com>
2020-08-17 15:54:33 +0100

    Added webhook module

--- webhook.js
+++ webhook.js
@@ -2,7 +2,7 @@
 
 module.exports = (app) => {
     app.post('/webhook', (req, res) => {
-        console.log('Activate webhook');
+        console.log('Activate Github Webhook');
         console.log(req.body);
         res.status(200).send({success: true});
     });
