c9b55489721ac274a3b88c6935352cff662cd80b
Kola Grey <kolagrey@gmail.com>
2020-08-17 05:40:43 +0100

    change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Asheori' });
+  res.render('index', { title: 'Asheori Reactive Technology' });
 });
 
 module.exports = router;
