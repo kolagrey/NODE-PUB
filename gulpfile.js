f75b16e35ee9112e8d7882c68bf2f48f3b036cf6
Kola Grey <kolagrey@gmail.com>
2020-08-18 14:20:43 +0100

    Added test setup

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,19 @@
 
 const gulp = require("gulp"),
   jshint = require("gulp-jshint"),
-  stylish = require("jshint-stylish");
+  stylish = require("jshint-stylish"),
+  mocha = require('gulp-mocha'),
+  gutil = require('gulp-util');
+
+gulp.task('mocha', function() {
+    return gulp.src(['test/*.js'], { read: false })
+        .pipe(mocha({ reporter: 'list' }))
+        .on('error', gutil.log);
+});
+
+gulp.task('watch-mocha', function() {
+    gulp.watch(['test/**'], ['mocha']);
+});
 
 const path = {
   js: ["./app.js", "./routes/*.js"],
